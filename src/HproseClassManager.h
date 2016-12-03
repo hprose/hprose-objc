57f89cb9ff520c82bdca9aae1476db9af774c06c
Ma Bingyao <mabingyao@gmail.com>
2016-12-04 00:10:20 +0800

    Added some comments

--- src/HproseClassManager.h
+++ src/HproseClassManager.h
@@ -12,18 +12,26 @@
  *                                                        *
  * hprose class manager header for Objective-C.           *
  *                                                        *
- * LastModified: Apr 10, 2014                             *
+ * LastModified: Dec 4, 2016                             *
  * Author: Ma Bingyao <andot@hprose.com>                  *
  *                                                        *
 \**********************************************************/
 
 #import <Foundation/Foundation.h>
 
+// Hprose class manager
 @interface HproseClassManager : NSObject
 
+// Register the class with alias.
 + (void) registerClass:(Class)cls withAlias:(NSString *)alias;
+
+// Get alias of class.
 + (NSString *) getClassAlias:(Class)cls;
+
+// Get class by alias.
 + (Class) getClass:(NSString *)alias;
+
+// Whether the class is registered with alias.
 + (BOOL) containsClass:(NSString *)alias;
 
 @end
