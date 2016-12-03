e8e029319d96825492ea290e768f35eb4f404109
Ma Bingyao <mabingyao@gmail.com>
2016-12-03 21:30:00 +0800

    Update swift example to swift 3.0

--- examples/swiftExam/swiftExam/main.swift
+++ examples/swiftExam/swiftExam/main.swift
@@ -4,34 +4,61 @@ class User:NSObject {
     var name: String = ""
     var age: Int = 0
     var married: Bool = false
-    var birthday: NSDate?
+    var birthday: Date?
     var sex: Int = 0
 }
 
-@objc protocol Hello {
-    func hello(name:String) -> String
-    func sum(a:Int, b:Int, c:Int) -> Int
-    func swapKeyAndValue(dict: [String: String]) -> [String: String]
-    func getUserList() -> Array<User>;
-}
-
 HproseClassManager.registerClass(User.self, withAlias: "User")
-var client = HproseHttpClient("http://www.hprose.com/example/index.php")
-var h = client.useService(Hello)
-client.invoke("hello", withArgs: ["async world"], settings: ["async": true]).done { (result) in
-    print(result);
-};
-print(h.hello("world"));
-print(h.hello("hprose"));
-print(h.hello("中文"));
-print(h.sum(1,b:2,c:3));
-print(h.swapKeyAndValue(["January": "Jan", "February": "Feb", "March": "Mar", "April": "Apr"]));
+var client = HproseHttpClient("http://www.hprose.com/example/index.php")!
+
+var result = client.asyncInvoke("hello", withArgs: ["async world"])
+
+result.done( { (result) in
+    print(result!);
+}, fail: {
+    (e) in
+    print(e!);
+});
+
+print(client.invoke("hello", withArgs: ["sync world"]));
+
 var sexStrings = ["Unknown", "Male", "Female", "InterSex"];
-var users = h.getUserList();
+
+var users = client.invoke("getUserList") as! [User];
 for user in users {
     print(user.name);
     print(user.age);
     print(user.married);
-    print(user.birthday);
+    print(user.birthday!);
+    print(sexStrings[user.sex]);
+}
+
+@objc protocol Hello {
+    func hello(_ name:String, _ callback: HproseCallback, _ errorBlock: HproseErrorCallback) -> Void
+    func sum(_ a:Int, _ b:Int, _ c:Int) -> NSNumber
+    func swapKeyAndValue(_ dict: [String: String]) -> [String: String]
+    func getUserList() -> Array<User>;
+}
+
+var proxy = client.useService(Hello.self) as AnyObject;
+
+proxy.hello("async world", {(result, args) in
+    print(result!);
+}, { (name, error) in
+    print(error!)
+})
+
+print(client.invoke("hello", withArgs: ["sync world"]));
+
+print(proxy.sum(1, 2, 3))
+
+print(proxy.swapKeyAndValue(["January": "Jan", "February": "Feb", "March": "Mar", "April": "Apr"]))
+
+var userList = proxy.getUserList();
+for user in userList {
+    print(user.name);
+    print(user.age);
+    print(user.married);
+    print(user.birthday!);
     print(sexStrings[user.sex]);
 }
